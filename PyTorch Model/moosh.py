1a295a0c7b5847e58c08c1c7daf33d35cc6d2c17
mathg15 <69726961+mathg15@users.noreply.github.com>
2022-04-21 16:46:51 +0100

    Update moosh.py

--- PyTorch Model/moosh.py
+++ PyTorch Model/moosh.py
@@ -2,55 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 
-
-def Layer(Epsilon, Mu, Height):
-    """
-    :param Epsilon: Permittivity of the layer
-    :param Mu: Permeability of the layer
-    :param Height: Height of the layer
-    :return: Array like [Epsilon, Mu, Height]
-    """
-    return np.array([[Epsilon, Mu, Height]])
-
-
-def Structure(*args):
-    """
-    Create a multi-layers structure
-
-    :param args: layers of the structure
-    :return: array for each parameter of the layers
-
-    Example :
-
-    Layer1 = [1,1,100]; Layer2 = [2,1,100]
-
-    Return : Eps = [1, 2]; Mu = [1, 1}; Height = [100, 100]
-    """
-    structure = args[0]
-    for i in range(len(args) - 1):
-        structure = np.append(structure, args[i + 1], axis=0)
-    epsStruc = structure[:, 0]
-    muStruc = structure[:, 1]
-    heightStruc = structure[:, 2]
-    return epsStruc, muStruc, heightStruc
-
-
-def StructureHeight(height, periods, addEpsAir=True):
+# Définition d'une structure avec les épaisseurs comme arguments 
+def StructureHeight(height, periods):
     Eps = np.tile([2, 3], (1, periods))
     Eps = np.insert(Eps, 0, 1)
     Eps = np.append(Eps, 1)
     Mu = np.ones(2 * periods)
     return Eps, Mu, height
 
-def StructureHeight_GAN(height, periods):
-    Eps = np.tile([2, 3], (1, periods))
-    Eps = np.insert(Eps, 0, 1)
-    Eps = np.append(Eps, 1)
-    Mu = np.ones(2 * periods + 2)
-    Height = np.insert(height, 0, 100)
-    Height = np.insert(Height, len(Height), 100)
-    return Eps, Mu, Height
-
+# Définition d'une structure avec des épaisseurs aléatoires
 def random_structure_imp(periods):
     Eps = np.tile([2, 3], (1, periods))
     Eps = np.insert(Eps, 0, 1)
@@ -65,32 +25,7 @@ def random_structure_imp(periods):
     Height = np.insert(Height, len(Height), 100)
     return Eps, Mu, Height
 
-def random_structure_test(periods):
-    Eps = np.tile([2, 3], (1, periods))
-    Eps = np.insert(Eps, 0, 1)
-    Eps = np.append(Eps, 1)
-    Mu = np.ones(2 * periods + 2)
-    Height = np.tile([600 / (4 * np.sqrt(2)), 600 / (4 * np.sqrt(3))], (1, periods))
-    Height = np.insert(Height, 0, 1600)
-    Height = np.append(Height, 100)
-    return Eps, Mu, Height
-
-
-def random_structure_gan(periods):
-    Eps = np.tile([2, 3], (1, periods))
-    Eps = np.insert(Eps, 0, 1)
-    Eps = np.append(Eps, 1)
-    Mu = np.ones(2 * periods + 2)
-    height = []
-    for i in range(2 * periods):
-        randHeight = np.random.randint(1, 350)
-        height.append(randHeight)
-    height = np.asarray(height)
-    Height = np.insert(height, 0, 1600)
-    Height = np.insert(Height, len(Height), 100)
-    return Eps, Mu, Height
-
-
+# Structure miroir de Bragg
 def StructureBragg(periods):
     Eps = np.tile([2, 3], (1, periods))
     Eps = np.insert(Eps, 0, 1)
@@ -104,7 +39,7 @@ def StructureBragg(periods):
 
     return Eps, Mu, Height
 
-
+# Calcul des coefficients de réflexion
 class Reflection:
 
     def __init__(self, structure):
@@ -152,25 +87,3 @@ class Reflection:
         re = np.abs(re) ** 2
         return re
 
-    def genSpectrumGAN(self):
-        rangeLambda = np.arange(400, 800, 1)
-        re = np.ones(400, dtype=complex)
-        print(re.shape)
-        print(rangeLambda.shape)
-        for i in range(len(re)):
-            lambda_ = rangeLambda[i]
-            re[i] = self.coefficient(lambda_)
-        re = np.abs(re) ** 2
-        return re
-
-
-def add_Height(hauteur):
-    Hauteur = np.insert(hauteur, 0, 1600)
-    Hauteur = np.insert(Hauteur, len(hauteur), 100)
-    return Hauteur
-
-
-def remove_Height(hauteur):
-    Hauteur = np.insert(hauteur, 0, 1600)
-    Hauteur = np.insert(Hauteur, len(hauteur), 100)
-    return Hauteur
